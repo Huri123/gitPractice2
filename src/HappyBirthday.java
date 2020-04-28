4727367a166d6c81dc9132171b6c26306b9d784e
Huri123 <64114505+Huri123@users.noreply.github.com>
2020-04-28 14:54:09 -0500

    Update HappyBirthday.java

--- src/HappyBirthday.java
+++ src/HappyBirthday.java
@@ -1,5 +1,8 @@
 public class HappyBirthday {
     public static void main(String[] args) {
         System.out.println("Happy birthday!");
+        
+        
+        System.out.println("Happy birthday!");//This line is added by github
     }
 }
